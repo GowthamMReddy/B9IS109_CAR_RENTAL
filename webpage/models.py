06546de829150209a297ca6aeaf609d329224a90
GowthamMReddy <117168448+GowthamMReddy@users.noreply.github.com>
2023-03-02 15:23:16 +0000

    created database successfully

--- webpage/models.py
+++ webpage/models.py
@@ -20,12 +20,12 @@ class Booking(db.Model):
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.string(100), unique=True)
-    first_name = db.Column(db.string(100))
-    last_name = db.Column(db.string(100))
-    contact = db.Column(db.Integer(10))
+    email = db.Column(db.String(100), unique=True)
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    contact = db.Column(db.Integer)
     dob = db.Column(db.DateTime())
-    password = db.Column(db.string(100))
+    password = db.Column(db.String(100))
     create_date = db.Column(db.DateTime(timezone=True), default=func.now())
     bookings = db.relationship('Booking')
 
